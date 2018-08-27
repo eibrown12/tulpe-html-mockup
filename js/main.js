cd9e1211007f1dbf9a8bd684152c38752278e0e3
Eric Brown <eibrown12@gmail.com>
2018-08-27 11:27:54 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function($) {
   "use strict"; // Start of use strict
-
+	
   // Floating label headings for the contact form
   $("body").on("input propertychange", ".floating-label-form-group", function(e) {
     $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
@@ -15,7 +15,7 @@
   
   $(document).scroll(function () {
 	var $nav = $(".bg-light");
-	$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	$nav.toggleClass('scrolled shadow-sm', $(this).scrollTop() > $nav.height());
   });
   //primary navigation slide-in effect
   /*if ($(window).width() > MQL) {
